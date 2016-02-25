f18a832c4d3c875aab23534490062670cad7e380
Julien Landuré <julien.landure@gmail.com>
2016-02-25 11:35:53 +0100

    feat(makefile): cpp files with openCV and so lib experimentation

---
+++ test1/DisplayConsole.hpp
@@ -0,0 +1,4 @@
+#ifndef __JLA_CONSOLE__
+#define __JLA_CONSOLE__
+int consoleElement();
+#endif
---
+++ test2/DisplayConsole.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int consoleElement()
+{
+        printf("Yeah2 !\n");
+        return 0;
+}
